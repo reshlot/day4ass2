74a82bbd834d83d11614852c70f9604d41125ad8
HP <reshmaksriet@gmail.com>
2023-07-13 15:34:03 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4exer2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Enter User Name: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter Password: ");
+                string pwd = Console.ReadLine();
+                if (name == "reshma" && pwd == "2417")
+                {
+                    Console.WriteLine("Login Successfull");
+                }
+                else if (name != "reshma")
+                {
+                    Console.WriteLine("Invalid UserName");
+                }
+                else if (pwd != "2417")
+                {
+                    Console.WriteLine("Invalid Password");
+                }
+                else if (name == "reshma" && pwd != "2417")
+                {
+                    Console.WriteLine("Invalid Password.");
+                }
+                Console.WriteLine("\n");
+            }
+            Console.WriteLine("Login Failed");
+            Console.ReadKey();
+        }
+    }
+}
